18ed4614106f190c935cd81e4d1248714041c908
Jochen Klar <mail@jochenklar.de>
2023-03-12 17:48:33 +0100

    Update scripts

--- clean.py
+++ clean.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
+import argparse
 import json
 import shutil
 from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--layer', default=None)
+
+args = parser.parse_args()
 
 with open('config.json') as fp:
     config = json.load(fp)
@@ -9,11 +15,12 @@ with open('config.json') as fp:
 base_path = Path('tmp')
 
 for layer in config['layers']:
-    for zoom in ['16', '17', '18', '19', '20']:
-        path = base_path / layer / zoom
-        print(path)
+    if args.layer in [None, layer]:
+        for zoom in ['16', '17', '18', '19', '20']:
+            path = base_path / layer / zoom
+            print(path)
 
-        try:
-            shutil.rmtree(path)
-        except FileNotFoundError:
-            pass
+            try:
+                shutil.rmtree(path)
+            except FileNotFoundError:
+                pass
--- seed.py
+++ seed.py
@@ -1,12 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+import argparse
 import json
 import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--layer', default=None)
+
+args = parser.parse_args()
 
 with open('config.json') as fp:
     config = json.load(fp)
 
 for layer in config['layers']:
-    for zoom in ['10', '11', '12', '13']:
-        cmd = ['tilestache-seed', '-c', 'config.json', '-l', layer,
-               '-b', '52.68', '13.07', '52.33', '13.77', zoom]
-        subprocess.check_call(cmd)
+    if args.layer in [None, layer]:
+        for zoom in ['10', '11', '12', '13']:
+            cmd = ['tilestache-seed', '-c', 'config.json', '-l', layer,
+                   '-b', '52.68', '13.07', '52.33', '13.77', zoom]
+            subprocess.check_call(cmd)
